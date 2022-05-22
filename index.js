f065652956bc20a8e78a2229716a75cc64532ad6
FrancoBrizuela01 <franco-alexis16@hotmail.com>
2022-05-22 20:43:10 -0300

    categories

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ const app = express();
 
 connectDB();
 
-// app.use(cors());
-
-const corsOptions = {
-  origin: "http://localhost:4000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ extended: true }));
 
